9116c4b1c64c4fe9b62dba1844fea3e73040b79b
pain-arch <himulol123@gmail.com>
2024-04-21 13:30:02 +0600

    Add SearchBar component, and Add Input component from shadcn ui

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,12 @@
+
+const SearchBar = () => {
+    return (
+        <div className="relative w-full h-14 flex flex-col bg-white">            
+            <div className="relative h-14 z-10 rounded-md">
+                
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar;
